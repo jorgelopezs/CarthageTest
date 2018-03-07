9c0c5bca475b6f5295cd77d070a5a6f402d0da02
Jorge <jorge@Jorges-MacBook.nyc.rr.com>
2018-03-06 22:53:59 -0500

    1st commit

---
+++ CarthageTest/CarthageTest.h
@@ -0,0 +1,19 @@
+//
+//  CarthageTest.h
+//  CarthageTest
+//
+//  Created by Jorge on 3/6/18.
+//  Copyright © 2018 Jorge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CarthageTest.
+FOUNDATION_EXPORT double CarthageTestVersionNumber;
+
+//! Project version string for CarthageTest.
+FOUNDATION_EXPORT const unsigned char CarthageTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageTest/PublicHeader.h>
+
+
---
+++ CarthageTest/MyClass.swift
@@ -0,0 +1,14 @@
+//
+//  MyClass.swift
+//  CarthageTest
+//
+//  Created by Jorge on 3/6/18.
+//  Copyright © 2018 Jorge. All rights reserved.
+//
+
+import Foundation
+
+
+public func test(name:String) -> String{
+    return "Value given is \(name)"
+}
